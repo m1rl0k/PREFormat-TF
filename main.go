2f59354b65958fc4b7a1987ea05998dfe6163ae0
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-02 11:51:34 -0400

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/fatih/color"
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclparse"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/pmezard/go-difflib/difflib"
 )
@@ -42,33 +41,7 @@ func processTerraformFile(filename string) {
 		return
 	}
 
-	file := hclwrite.NewEmptyFile()
-	newBody := file.Body()
-
-	content, diags := f.Body.Content(&hcl.BodySchema{})
-	if diags.HasErrors() {
-		fmt.Println("Error decoding body:", filename, diags)
-		return
-	}
-
-	for name, attr := range content.Attributes {
-		tokens := tokensForExpr(attr.Expr, f)
-		newBody.SetAttributeRaw(name, tokens)
-	}
-
-	for _, block := range content.Blocks {
-		newBlock := newBody.AppendNewBlock(block.Type, block.Labels)
-		blockContent, _ := block.Body.Content(&hcl.BodySchema{})
-		for name, bAttr := range blockContent.Attributes {
-			tokens := tokensForExpr(bAttr.Expr, f)
-			newBlock.Body().SetAttributeRaw(name, tokens)
-		}
-		for _, subBlock := range blockContent.Blocks {
-			writeBlock(subBlock, newBlock.Body())
-		}
-	}
-
-	formattedData := file.Bytes()
+	formattedData := hclwrite.Format(data)
 
 	if string(data) != string(formattedData) {
 		diff := difflib.UnifiedDiff{
@@ -89,61 +62,3 @@ func processTerraformFile(filename string) {
 		fmt.Printf("No changes needed for %s\n", filename)
 	}
 }
-
-func writeBlock(block *hcl.Block, body *hclwrite.Body) {
-	newBlock := body.AppendNewBlock(block.Type, block.Labels)
-	blockContent, _ := block.Body.Content(&hcl.BodySchema{})
-	for name, bAttr := range blockContent.Attributes {
-		tokens := tokensForExpr(bAttr.Expr, nil)
-		newBlock.Body().SetAttributeRaw(name, tokens)
-	}
-	for _, subBlock := range blockContent.Blocks {
-		writeBlock(subBlock, newBlock.Body())
-	}
-}
-
-func tokensForAttr(attr *hclsyntax.Attribute, f *hcl.File) hclwrite.Tokens {
-	var tokens hclwrite.Tokens
-
-	tokens = append(tokens, hclwrite.Token{
-		Type:  hclwrite.TokenIdent,
-		Bytes: []byte(attr.Name),
-	})
-
-	return tokens
-}
-
-func tokensForExpr(expr hcl.Expression, f *hcl.File) hclwrite.Tokens {
-	var tokens hclwrite.Tokens
-
-	switch expr := expr.(type) {
-	case *hclsyntax.LiteralValueExpr:
-		tokens = append(tokens, hclwrite.Token{Type: hclwrite.TokenString, Bytes: []byte(expr.Val.AsBigFloat().String())})
-
-	case *hclsyntax.TemplateExpr:
-		parts := expr.Parts
-		for _, part := range parts {
-			switch part := part.(type) {
-			case *hclsyntax.TemplateExpr:
-				tokens = append(tokens, tokensForExpr(part, f)...)
-
-			case *hclsyntax.TemplateWrapExpr:
-				tokens = append(tokens, tokensForExpr(part.Wrapped, f)...)
-
-			case *hclsyntax.LiteralValueExpr:
-				tokens = append(tokens, hclwrite.Token{Type: hclwrite.TokenString, Bytes: []byte(part.Val.AsBigFloat().String())})
-			}
-		}
-
-	case *hclsyntax.VariableExpr:
-		tokens = append(tokens, hclwrite.Token{Type: hclwrite.TokenIdent, Bytes: []byte("$")})
-
-		attrTokens := tokensForAttr(&hclsyntax.TraverseAttr{Name: expr.Name}, f)
-		tokens = append(tokens, attrTokens...)
-	// ... rest of the cases and default ...
-	}
-
-	return tokens
-}
-
-
