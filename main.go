f06a11839518bf43e4247084c55e13562d230d8d
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-02 10:46:03 -0400

    Update main.go

--- main.go
+++ main.go
@@ -89,8 +89,8 @@ func processTerraformFile(filename string) {
 func tokensForExpr(expr hcl.Expression, f *hcl.File) hclwrite.Tokens {
     return hclwrite.Tokens{
         {
-            Type:  hcl.TokenIdent,
-            Bytes: []byte(expr.Range().Start.Content),
+            Type:  hclsyntax.TokenIdent,
+            Bytes: []byte(expr.Range().Start.ByteSlice(data)),
             Pos: hclwrite.Pos{
                 Filename: f.Pos.Filename,
                 Offset:   int(expr.Range().Start.Byte),
